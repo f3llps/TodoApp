fd012512025caef7c3480e9eb37f72c22a24bdf5
f3llps <fellipeversiani@gmail.com>
2018-12-01 07:51:24 -0200

    Desenvolvimento do backend com Express

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default props => (
-
     <nav className='navbar navbar-inverse'>
         <div className='container'>
             <div className='navbar-header'>
